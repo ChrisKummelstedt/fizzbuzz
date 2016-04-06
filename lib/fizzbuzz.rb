2a496cc0207b4a594c20752f1bfc6a1946d14f6e
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-06 14:55:25 +0200

    We fixed the error

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,12 @@
-fizzbuzz(number)
+def fizzbuzz(number)
  	if number%3 == 0
 		 "fizz"
-	elsif number%5 == 0 
+	elsif number%5 == 0
 		 "buzz"
 	elsif (number%5 == 0 && number%3 == 0)
 		 "fizzbuzz"
-	else 
-		 number
+	else
+		  number
 	end
+
 end
