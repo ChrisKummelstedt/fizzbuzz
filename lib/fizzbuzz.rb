b2553cb119804f6c49ea8ab9bb21d9ea86350588
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-06 15:32:06 +0200

    fizzbuzz like pros

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,5 @@
 def fizzbuzz(number)
- 	if number%3 == 0
-		 "fizz"
-	elsif number%5 == 0
-		 "buzz"
-	elsif (number%5 == 0 && number%3 == 0)
-		 "fizzbuzz"
-	else
-		  number
-	end
+
+number%5 != 0 && number%3 != 0 ? number : number%5 == 0 && number%3 == 0 ? 'fizzbuzz' : number%3 == 0 ? 'fizz' : 'buzz'
 
 end
